767e9bca436d0a2cac51254af698c6f846996752
bols <joelmer13@gmail.com>
2021-05-31 21:00:50 -0400

    adjusted grid color

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -11,8 +11,8 @@ function drawSquareGrid(x) {
     gridContainer.style['grid-template-columns'] = `repeat(${x}, 1fr)`;
     for (let i = 0; i < gridArea; i++) {
         let gridSquare = document.createElement("div");
-        gridSquare.style['background-color'] = `hsl(200, 75%, 75%)`;
-        gridSquare.style['border'] = `3px solid black`;
+        gridSquare.style['background-color'] = `hsl(200, 15%, 95%)`;
+        gridSquare.style['border'] = `1px dotted hsl(0, 0%, 85%)`;
         gridContainer.appendChild(gridSquare);
     }
 }
