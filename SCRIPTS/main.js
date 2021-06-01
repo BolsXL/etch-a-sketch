b7a620b121cad15964b9eece8882430222798fdd
bols <joelmer13@gmail.com>
2021-05-31 21:55:17 -0400

    Added drawing functionality

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -2,7 +2,7 @@
 const gridContainer = document.querySelector(".page-wrap");
 
 // RUNNING CODE
-drawSquareGrid(64);
+drawSquareGrid(32);
 
 // FUNCTION
 function drawSquareGrid(x) {
@@ -13,6 +13,16 @@ function drawSquareGrid(x) {
         let gridSquare = document.createElement("div");
         gridSquare.style['background-color'] = `hsl(200, 15%, 95%)`;
         gridSquare.style['border'] = `1px dotted hsl(0, 0%, 85%)`;
+        gridSquare.style['width'] = '100%';
+        gridSquare.style['height'] = '100%';
+        gridSquare.addEventListener('mousedown', (e) => {
+            gridSquare.classList.toggle("fill");
+        });
+        gridSquare.addEventListener('mouseover', (e) => {
+            if (e.buttons == 1) {
+                gridSquare.classList.toggle("fill");
+            }
+        });
         gridContainer.appendChild(gridSquare);
     }
 }
